e1a91f9bc5ca61187e0476fe2f336624c843a901
Porta <portalmaster137@gmail.com>
2023-03-14 01:22:37 +1000

    rickroll heheheheheheh

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,12 @@ use tide::{Request, prelude::*};
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
     app.at("/").get(hello_world);
-    app.at("/secret").get(secret);
     println!("Listening on 80");
     app.listen("0.0.0.0:80").await?;
     Ok(())
 }
 
 async fn hello_world(_: Request<()>) -> tide::Result {
-    Ok("Hello, oh penis!".into())
+    Ok(tide::Redirect::new("https://www.youtube.com/watch?v=H8ZH_mkfPUY").into())
 }
 
-async fn secret(_: Request<()>) -> tide::Result {
-    Ok("Secret!".into())
-}

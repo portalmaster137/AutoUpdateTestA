6a37abd3afff4a9b5a6b3276294d4d97a1c7d0eb
Porta <portalmaster137@gmail.com>
2023-03-14 00:44:22 +1000

    Aaa

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ async fn main() -> tide::Result<()> {
     let mut app = tide::new();
     app.at("/").get(hello_world);
     println!("Listening on 80");
-    app.listen("127.0.0.1:80").await?;
+    app.listen("0.0.0.0:80").await?;
     Ok(())
 }
 

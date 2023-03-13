b16d0fa16a46f13b6ef2c4bce8aeb40dc4532bfe
Porta <portalmaster137@gmail.com>
2023-03-14 00:27:31 +1000

    basic http

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-fn main() {
-    println!("Hello, world!");
-    println!("I'm a Rustacean!");
-    println!("boo.");
+use tide::{Request, prelude::*};
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/").get(hello_world);
+    println!("Listening on 8080");
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+async fn hello_world(_: Request<()>) -> tide::Result {
+    Ok("Hello, world!".into())
 }

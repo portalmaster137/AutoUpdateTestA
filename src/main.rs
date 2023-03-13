c24d1f7fb424aedc5fbc117d6c4f6ea4f4bdc9eb
Porta <portalmaster137@gmail.com>
2023-03-14 01:09:48 +1000

    Aaa

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use tide::{Request, prelude::*};
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
     app.at("/").get(hello_world);
+    app.at("/secret").get(secret);
     println!("Listening on 80");
     app.listen("0.0.0.0:80").await?;
     Ok(())
@@ -11,4 +12,8 @@ async fn main() -> tide::Result<()> {
 
 async fn hello_world(_: Request<()>) -> tide::Result {
     Ok("Hello, oh penis!".into())
+}
+
+async fn secret(_: Request<()>) -> tide::Result {
+    Ok("Secret!".into())
 }
